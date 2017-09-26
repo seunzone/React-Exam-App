ca1d3041b86672b2a510f68116ebfc4f5c4c8005
seunzone <darealseun@gmail.com>
2017-09-26 22:58:43 +0100

    Working with the constructor object

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,31 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            questions: [
+                {
+                    id:1,
+                    text: 'What is your name?',
+                    choices: [
+                        {
+                            id: 'a',
+                            text: 'Mike'
+                        },
+                        {
+                            id: 'b',
+                            text: 'Seun'
+                        },
+                        {
+                            id: 'c',
+                            text: 'Li'
+                        }
+                    ]
+                }
+            ]
+        }
+    }
         render(){
         return(
             <div>
