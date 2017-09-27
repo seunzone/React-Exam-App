486d77e70553d75d3d13e10369b090106e6dbedd
seunzone <darealseun@gmail.com>
2017-09-27 11:08:44 +0100

    Added Question Components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import QuestionList from './quiz/QuestionList.jsx';
 
 class App extends Component{
     constructor(props){
@@ -22,15 +23,75 @@ class App extends Component{
                             id: 'c',
                             text: 'Li'
                         }
-                    ]
+                    ],
+                    correct: 'b'
+                },
+                {
+                    id:2,
+                    text: 'What is your Stack?',
+                    choices: [
+                        {
+                            id: 'a',
+                            text: 'JavaScript'
+                        },
+                        {
+                            id: 'b',
+                            text: 'Python'
+                        },
+                        {
+                            id: 'c',
+                            text: 'C#'
+                        }
+                    ],
+                    correct: 'a'
+                },
+                {
+                    id:3,
+                    text: 'Who is Your Role Model',
+                    choices: [
+                        {
+                            id: 'a',
+                            text: 'James Fisher'
+                        },
+                        {
+                            id: 'b',
+                            text: 'Colt Steele'
+                        },
+                        {
+                            id: 'c',
+                            text: 'Wes Bos'
+                        }
+                    ],
+                    correct: 'c'
+                },
+                {
+                    id:4,
+                    text: 'Where would you like to live?',
+                    choices: [
+                        {
+                            id: 'a',
+                            text: 'China'
+                        },
+                        {
+                            id: 'b',
+                            text: 'Silicon Valley'
+                        },
+                        {
+                            id: 'c',
+                            text: 'Evil Forest'
+                        }
+                    ],
+                    correct: 'b'
                 }
-            ]
+            ],
+            score: 0,
+            current: 1
         }
     }
         render(){
         return(
             <div>
-                Boom
+                <QuestionList {...this.state}/>
             </div>
         )
     }
